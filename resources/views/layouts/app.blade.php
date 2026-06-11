c81f0068d119bde9c60e328d42632b2b11f83508
SugantaInternation <suganta1@gmail.com>
2026-06-11 17:02:42 +0530

    feat: add Google AdSense script for improved monetization

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,6 +91,8 @@ $routeName = request()->route()?->getName();
 
         gtag('config', 'AW-17989742944');
     </script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5147720230554161"
+     crossorigin="anonymous"></script>
 </head>
 
 <body class="min-h-screen flex flex-col bg-surface text-text-main">
